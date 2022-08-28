e084d14b7258abe0741bab72b638568e784c7077
TensaCoder <herschel.menezes00@gmail.com>
2022-08-28 22:09:33 +0530

    Hashed password to store in DB

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -9,6 +9,7 @@ const Router = require('express')
 const router = Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 
 // Create a user using POST at "/api/auth/createuser". Doesnt require auth
@@ -19,18 +20,37 @@ router.post('/create-user',
     body('email').isEmail(),
     body('password', 'Password should be atleast 8 character').isLength({min : 8})
 ], 
-(req, res)=>{
+async(req, res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email : req.body.email,
-        password: req.body.password
-      })
-      .then(user => res.json(user));
-});
 
+    try {
+        let user = await User.findOne({email: req.body.email});
+        if (user){
+            return res.status(400).json({error : "A User with the email already exists!!!"})
+        }
+
+        // Once detail validation is done, we go on to encrypt the password and store the new encrypted password on the database using salt and hash function.
+        let salt = bcrypt.genSaltSync(10);
+        console.log(salt);
+        let hash = bcrypt.hashSync(req.body.password, salt);
+
+        // creating the user in the database
+        User.create({
+            name: req.body.name,
+            email : req.body.email,
+            password: hash
+        })
+        .then(user => res.json(user));
+        
+    } catch (err) {
+        return res.status(500).json({ error: err.message})
+        
+    }
+    
+    
+});
 
 module.exports = router
