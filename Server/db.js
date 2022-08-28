2eae46d4e7de997959281f449cc4dd3878a70389
TensaCoder <herschel.menezes00@gmail.com>
2022-08-28 17:47:58 +0530

    Express-validator used and email index created

--- Server/db.js
+++ Server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-CONNECTION_URL = `mongodb+srv://${process.env.UserName}:${process.env.DB_Password}@tensacoder.85rj5.mongodb.net/Notes?retryWrites=true&w=majority`
+CONNECTION_URL = `mongodb+srv://TensaCoder:${process.env.DB_Password}@tensacoder.85rj5.mongodb.net/Notes?retryWrites=true&w=majority`
 // console.log("COnnection url value : ",CONNECTION_URL);
 const connectToMongo = () =>{
     mongoose.connect(CONNECTION_URL, () =>{
--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -8,16 +8,29 @@
 const Router = require('express')
 const router = Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 
 // Create a user using POST at "/api/auth/createuser". Doesnt require auth
 
-router.post('/createuser', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.json(req.body);
-})
+router.post('/create-user',
+[
+    body('name').isLength({min : 3}),
+    body('email').isEmail(),
+    body('password', 'Password should be atleast 8 character').isLength({min : 8})
+], 
+(req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email : req.body.email,
+        password: req.body.password
+      })
+      .then(user => res.json(user));
+});
 
 
 module.exports = router
