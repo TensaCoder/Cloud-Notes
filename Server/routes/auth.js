da5000ba7543c64d6038a5fa12159fd48c434515
TensaCoder <herschel.menezes00@gmail.com>
2022-09-02 23:23:53 +0530

    /getuser endpoint created

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -7,11 +7,11 @@
 // Option 2
 const Router = require('express')
 const router = Router();
-const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const User = require('../models/User');
+const fetchuser = require('../middleware/fetchuser')
 
 // Create a user using POST at "/api/auth/createuser". Doesnt require auth
 
@@ -50,7 +50,7 @@ router.post('/create-user',
                     id: user.id
                 }
             }
-            const authToken = jwt.sign(data, process.env.JWT_SECRET)
+            const authToken = jwt.sign(data, process.env.JWT_Secret)
             // console.log(data);
 
             // Send the AuthToken to the User
@@ -113,15 +113,15 @@ router.post('/login',
     });
 
 // 3: Get LoggedIn user details using POST : /api/auth/getuser
-router.post('/getuser',
-    async (req, res) => {
-        try {
-            userId = "todo";
-            const user = await User.findById(userId).select("-password")
-        } catch (error) {
-            return res.status(500).json({ error: "Some Internal Error Occured!!!" });
-        }
-    });
+router.post('/getuser', fetchuser, async (req, res) => {
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password")
+        res.send(user)
+    } catch (error) {
+        return res.status(500).json({ error: "Some Internal Error Occured!!!" });
+    }
+});
 
 
 module.exports = router
