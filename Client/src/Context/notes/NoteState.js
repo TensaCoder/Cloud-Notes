06269e3a748bf9c98fc2f1eb6e288c40eb6005a1
TensaCoder <herschel.menezes00@gmail.com>
2022-10-11 16:40:09 +0530

    Note Context modified

---
+++ Client/src/Context/notes/NoteState.js
@@ -0,0 +1,26 @@
+// import react from "react";
+import NoteContext from "./NoteContext";
+
+const NoteState = (props) => {
+    const notes = [
+        {
+            _id: "123456",
+            title: "My first note",
+            description: "This is my first note",
+            tag: "personal",
+        },
+        {
+            _id: "1234567",
+            title: "My second note",
+            description: "This is my second note",
+            tag: "work",
+        },
+    ];
+
+    return (
+        <NoteContext.Provider value={notes}>
+            {props.children}
+        </NoteContext.Provider>
+    );
+}
+export default NoteState;
