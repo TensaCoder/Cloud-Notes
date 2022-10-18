1f2be5ab676949147fa02f0930ee19d232f387a6
TensaCoder <herschel.menezes00@gmail.com>
2022-10-18 21:06:52 +0530

    Application wrapped in NoteState Component

--- Client/src/App.js
+++ Client/src/App.js
@@ -7,23 +7,24 @@ import {
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 import About from './Components/About';
-import NoteState from './Context/notes/NoteState';
 import Notes from './Components/Notes';
+// import NoteContext from './Context/notes/NoteContext'; 
+import NoteState from './Context/notes/NoteState';
 
 
 function App() {
   return (
     <>
-    <NoteState>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/notes" element={<Notes />} />
-        </Routes>
-      </Router>
-    </NoteState>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/notes" element={<Notes />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
