2ca45de8d2136ea37a60d2cd78033b5e3d8f208d
TensaCoder <herschel.menezes00@gmail.com>
2023-01-10 01:16:04 +0530

    Adding Error handling

--- Client/src/Components/NotesAddition.js
+++ Client/src/Components/NotesAddition.js
@@ -1,37 +1,86 @@
-import React, {useContext, useState} from 'react';
-import NoteContext from "./NoteContext";
+import React, { useContext, useState } from 'react';
+import NoteContext from '../Context/notes/NoteContext';
+import Alert from './Alert';
 
 const NotesAddition = () => {
 
     // write a function to add a new note to the list
     const context = useContext(NoteContext);
-    const {addNotes} = context;
+    const { addNotes } = context;
 
-    const [note, setNote] = useState({title: "", description: "", tag: ""});
+    const [note, setNote] = useState({ title: " ", description: " ", tag: " " });
+    const [messages, setMessages] = useState("");
+    const [alert, setAlert] = useState(false)
 
-    const onChange = (event) =>{
-        setNote([...note], [event.target.name]:[event.target.value])
+    const onChange = (event) => {
+        setNote({ ...note, [event.target.name]: [event.target.value] })
+        // console.log(note)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        if (note.title[0].length < 5 || note.description[0].length < 5) {
+            // console.log("Handling note addition error")
+            // console.log(note.title, note.title[0].length)
+            // console.log(note.description, note.description[0].length)
+
+            // Set the alert value to TRUE
+            setAlert(true);
+
+            // Check condition and set appropriate error messages
+            if (note.title[0].length < 4 && note.description[0].length < 4) {
+                setMessages("Minimum length of title and description should be 5")
+                // console.log("Inside title and desc error")
+            }
+            else if (note.description[0].length < 4) {
+                setMessages("Minimum length of description should be 5")
+                // console.log("Inside desc error")
+            }
+            else if (note.title[0].length < 4) {
+                setMessages("Minimum length of title should be 5")
+                // console.log("Inside title error") 
+            }
+
+        }
+        else {
+            // console.log("Note added")
+            // Adding Notes
+            addNotes(note.title, note.description, note.tag);
+            setNote({ title: " ", description: " ", tag: " " })
+            setAlert(false)
+        }
+
     }
 
     return (
         <>
-            {/* Add a bootstrap component to take notes as input from the user */}
+            {/* Alert Component */}
+            {alert && <Alert message={messages} setAlert={setAlert} />}
+
             <div className="container my-3">
-                <h3>Add a Note</h3>
+                <h3 className='my-4'>Add a Note</h3>
                 <form>
+
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="title" />
+                        <input type="text" className="form-control" id="title" name='title' onChange={onChange} value={note.title} />
                     </div>
+
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Description</label>
-                        <input type="text" className="form-control" id="description" />
+                        <input type="text" className="form-control" id="description" name='description' onChange={onChange} value={note.description} />
                     </div>
+
                     <div className="mb-3">
                         <label htmlFor="tag" className="form-label">Tags</label>
-                        <input type="text" className="form-control" id="tag" />
+                        <input type="text" className="form-control" id="tag" name='tag' onChange={onChange} value={note.tag} />
                     </div>
-                    <button type="submit" className="btn btn-primary">Add Note</button>
+
+
+                    <div className='d-flex justify-content-center'>
+                        <button type="submit" className="btn btn-primary" onClick={onSubmit}>Add Note</button>
+                    </div>
                 </form>
             </div>
         </>
