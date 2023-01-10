b19314954b215add11a0b0e1d00b233df15e2f7a
TensaCoder <herschel.menezes00@gmail.com>
2023-01-10 22:39:47 +0530

    Alert Component merged with NoteItem errors

--- Client/src/Components/Alert.js
+++ Client/src/Components/Alert.js
@@ -3,13 +3,9 @@ import React from 'react'
 const Alert = (props) => {
     return (
         <>
-            {/* <div class="alert alert-danger" role="alert">
+            <div className="alert alert-danger alert-dismissible fade show" role="alert">
                 {props.message}
-            </div> */}
-
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {props.message}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=>{props.setAlert(false)}}></button>
             </div>
         </>
     )
--- Client/src/Components/NoteItem.js
+++ Client/src/Components/NoteItem.js
@@ -5,7 +5,7 @@ const NoteItem = (props) => {
     return (
         <>
             {/* display all the notes present*/}
-
+            
             {/* <div className='col-md-3 row-cols-3'> */}
             <div className="col-md-3">
             <div className="card my-3 mx-3">
@@ -14,7 +14,8 @@ const NoteItem = (props) => {
                         <h5 className="card-title">{note.title}</h5>
                         <i className="far fa-trash-alt ms-5 pb-2"></i>
                         <i className="far fa-edit mx-2 pb-2"></i>
-                        
+                        {/* <i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id)}}></i>
+                        <i className="far fa-edit mx-2" onClick={()=>{updateNote(note)}}></i> */}
                     </div>
                     <p className="card-text">{note.description}</p>
 
