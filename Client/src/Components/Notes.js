6181f5090d06957a6ec19b2928609fd9ae917d58
TensaCoder <herschel.menezes00@gmail.com>
2023-01-11 22:42:21 +0530

    Notes component linked with NoteAddition and NoteItem

--- Client/src/Components/Notes.js
+++ Client/src/Components/Notes.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import NotesAddition from './NotesAddition'
+import NoteItem from './NoteItem'
+import NoteContext from '../Context/notes/NoteContext';
 
-const Notes = () => {
-  return (
-    <div>Notes</div>
-  )
+const Notes = (props) => {
+    const context = useContext(NoteContext);
+    const { notes } = context;
+    return (
+        <>
+            <NotesAddition />
+
+            <div className='container'>
+                <hr></hr>
+                <h3>Your Notes</h3>
+                <div className='container d-flex flex-wrap'>
+                    {notes.map((note) => {
+                        return <NoteItem note={note} key={note._id} />
+                    })}
+                </div>
+
+            </div>
+            {/* <NoteItem /> */}
+        </>
+    )
 }
 
 export default Notes
