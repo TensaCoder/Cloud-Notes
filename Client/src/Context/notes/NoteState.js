b64c5c10ffde305a487539e03fd59ad9620b55d2
TensaCoder <herschel.menezes00@gmail.com>
2023-01-12 22:51:43 +0530

    Minor changes

--- Client/src/Context/notes/NoteState.js
+++ Client/src/Context/notes/NoteState.js
@@ -1,8 +1,9 @@
-// import react from "react";        check y this is not working
+// import react from "react";        //check y this is not working
 import NoteContext from "./NoteContext";
+import { useState } from "react";
 
 const NoteState = (props) => {
-    const notes = [
+    const initialNotes = [
         {
             _id: "123456",
             title: "My first note",
@@ -13,18 +14,50 @@ const NoteState = (props) => {
             _id: "1234567",
             title: "My second note",
             description: "This is my second note",
+            tag: "work", 
+        },
+        {
+            _id: "1234568",
+            title: "My third note",
+            description: "This is my third note",
             tag: "work",
         },
         {
-            _id: "1234567",
+            _id: "1234568",
+            title: "My third note",
+            description: "This is my third note",
+            tag: "work",
+        },
+        {
+            _id: "1234568",
+            title: "My third note",
+            description: "This is my third note",
+            tag: "work",
+        },
+        {
+            _id: "1234568",
             title: "My third note",
             description: "This is my third note",
             tag: "work",
         }
     ];
 
+    const [notes, setNotes] = useState(initialNotes);
+
+    const addNotes = (title, description, tag) =>{
+        // TODO : API Call
+
+        const note = {
+            title: title,
+            description: description,
+            tag: tag
+        }
+        setNotes(previousNotes => previousNotes.concat(note));
+    }
+
+
     return (
-        <NoteContext.Provider value={notes}>
+        <NoteContext.Provider value={{notes, addNotes}}>
             {props.children}
         </NoteContext.Provider>
     );
